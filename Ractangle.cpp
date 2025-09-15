b1cc8f8eb3e6a132eda0c01621bdb9b531fe0f11
정집인 <67492641+jaban0424@users.noreply.github.com>
2025-09-15 12:39:16 +0900

    Add files via upload

---
+++ Ractangle.cpp
@@ -0,0 +1,5 @@
+#include "Rectangle.h"
+
+int Rectangle::getArea() {
+	return width * height; // 멤버 변수 사용하면 괄호에 int width 쓰지 않아도 됨.
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "Rectangle.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Rectangle r;
+	r.width = 3;
+	r.height = 3;
+	cout<<r.getArea();
+}
